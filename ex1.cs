09249a038fae49b9a3727956a7b8526c045c7fa3
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-04-20 16:56:18 -0300

    Update ex1.cs

--- ex1.cs
+++ ex1.cs
@@ -1,12 +1,12 @@
 //  Faça um programa que apresente um menu inicial com as seguintes opções:
 //	MENU PRINCIPAL
-//1 – Funções sem vetor
+//  1 – Funções sem vetor
 //	2 – Funções com vetor
 //	Digite a opção desejada: _
 
 //  Se o usuário escolher a opção 1, solicite um número inteiro inicial e um número inteiro final. Em seguida apresente um segundo menu com as seguintes opções:
 //  MENU SECUNDÁRIO
-//1 - Inteiros em ordem crescente
+//  1 - Inteiros em ordem crescente
 //  2 - Inteiros em ordem decrescente
 //  3 - Inteiros ímpares (crescente)
 //  4 - Somatório dos inteiros
